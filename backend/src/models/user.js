3dc95d6d59754d0b3715d834fcc85521c668ea6a
jasonttyuen <jasonttyuen@gmail.com>
2022-03-23 19:46:45 +0800

    Added updateFriendlist

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,152 +1,540 @@
-const { ObjectID } = require('mongodb');
-
 const getDatabase = require('../util/database').getDatabase;
+const User = require('../models/user')
+const EmailSender = require('../util/emailSender')
+const crypto = require("crypto");
+const path = require("path");
+const { type } = require('os');
 
-class User {
-    static findAllAsync = async () => {
-        const db = getDatabase();
-        const result = await db.collection('user').find();
-        return result.toArray();
+const emailSender = new EmailSender("http://localhost:3000/verify?m=%email%&id=%id%")
+const pendingAccount = {};
+const pendingLogin = {};
+const pendingResetPassword = {};
+
+exports.createNewAccount = async (req, res, next) => {
+    const email = req.body.email
+    const name = req.body.name
+    const password = req.body.password
+
+    var user;
+    try {
+        user = await User.findByEmail(email, 'login')
+    } catch(e) {
     }
 
-    static findById = async (id, usage) => {
-        const db = getDatabase();
-        return await db
-            .collection('user')
-            .find({ '_id': ObjectID(id) })
-            .next()
-            .then(data => {
-                switch (usage) {
-                    case 'update':
-                        var user = new User(data.email, data.name);
-                        user.id = data._id;
-                        user.preferences = data.preferences
-                        return user;
-                    case 'query':
-                        return data;
-
-                    default:
-                        return data;
-                }
-                
-            })
-            .catch(err => {
-                throw err; 
-            });
+    if (user) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "This email is already in use."
+        }, null, "\t"));
+        res.end();
+        return
     }
 
-    static findByEmail = async (email, usage) => {
-        const db = getDatabase();
-        return await db
-            .collection('user')
-            .find({ email: email })
-            .next()
-            .then(data => {
-                switch (usage) {
-                    case 'register', 'login':
-                        var user = new User(data.email, data.name);
-                        user.id = data._id;
-                        user.status = data.status;
-                        return user;
-                    case 'all':
-                        var user = new User(data.email, data.name);
-                        user.id = data._id;
-                        user.status = data.status;
-                        user.preferences = data.preferences
-                        return user;
-                    default:
-                        return data;
-                }
-            })
-            .catch(err => {
-                throw err; 
-            });
-    }
+    const activateId = crypto.randomBytes(20).toString('hex');
+    pendingAccount[email] = { 'email': email, 'name': name, 'password': password, 'activateId': activateId };
 
-    static findByEmailAndPassword = async (email, password) => {
-        const db = getDatabase();
-        return await db
-            .collection('user')
-            .find({ 'email': email, 'password': password })
-            .next()
-            .then(data => {
-                const user = new User(data.email, data.name);
-                user.id = data._id;
-                user.status = data.status
-                return user;
-            })
-            .catch(err => {
-                throw err; 
-            });
-    }
+    // Set expire
+    setTimeout((email, id) => {
+        if (!(email in pendingAccount)) return;
+        if (pendingAccount[email].activateId == id)
+            delete pendingAccount[email]
+    }, 15 * 60 * 1000, email, activateId);
 
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-        // for personal self introduction
-        // example:
-        // reputation
-        // interest
-        // age....
+    emailSender.sendAccountActivation(email, name, activateId, function(error, info) {
+        if (error) 
+            console.log(error);
+        else       
+            console.log(info.response);
+    });
 
-    }
+    res.write(JSON.stringify({
+        "success": true
+    }, null, "\t"));
+    res.end();
 
-    async create() {
-        const db = getDatabase();
-        return db.collection('user').insertOne(this).then(result => {this.id = result.insertedId});
-    }
-
-    update() {
-        const db = getDatabase();
-        return db.collection('user').updateOne( { _id: this.id },
-                                                { $set: this },
-                                                { upsert: false })
-    }
-
-    updatePreferences() {
-        const db = getDatabase();
-        return db.collection('user').updateOne( { _id: this.id },
-                                                { $set: {
-                                                    'preferences': this.preferences
-                                                } },
-                                                { upsert: false })
-    }
-
-    updateProfilePicture() {
-        const db = getDatabase();
-        return db.collection('user').updateOne( { _id: this.id },
-                                                { $set: {
-                                                    'picture': this.picture
-                                                } },
-                                                { upsert: false })
-    }
-
-    updateName() {
-        const db = getDatabase();
-        return db.collection('user').updateOne( { _id: this.id },
-                                                { $set: {
-                                                    'name': this.name
-                                                } },
-                                                { upsert: false })
-    }
-
-    updatePassword() {
-        const db = getDatabase();
-        return db.collection('user').updateOne( { _id: this.id },
-                                                { $set: {
-                                                    'password': this.password
-                                                } },
-                                                { upsert: false })
-    }
-
-
-    //get the list of blocklist
-    //for compare , if block=>can't login
-    //else, can log in
-    getBlocklist(id){
-        
-    } 
-
+    console.log('email', email);
+    console.log('activateId', activateId);
 }
 
-module.exports = User;
+exports.activateAccount = async (req, res, next) => {
+    const email = req.query.m
+    const activateId = req.query.id
+
+    if (!(email in pendingAccount) || pendingAccount[email].activateId != activateId) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "This activation has expired."
+        }, null, "\t"));
+        res.end();
+        return
+    }
+
+    const user = new User(pendingAccount[email].email, pendingAccount[email].name);
+    user.password = pendingAccount[email].password
+    user.status = 'active'
+    delete pendingAccount[email];
+
+    try {
+        await user.create();
+    } catch (e) {
+        res.write(JSON.stringify(e, null, "\t"))
+        res.end()
+        return
+    }
+
+    res.write(JSON.stringify({
+        "success": true,
+        "message": "Your account is activated."
+    }, null, "\t"));
+    res.end()
+}
+
+function alreadyLogin(req, res, email) {
+    const loginSession = req.session.verification;
+    if (loginSession) 
+        if (loginSession.email == email && loginSession.verified) {
+            res.write(JSON.stringify({
+                "success": true,
+                "message": "You already login",
+                "user": { 'id': loginSession.id, 'email': email, 'name': loginSession.name }
+            }, null, "\t"));
+            res.end();
+            return true;
+        }
+
+    return false;
+}
+
+exports.login = async (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    if (alreadyLogin(req, res, email)) return
+
+    var user;
+    try {
+        user = await User.findByEmailAndPassword(email, password)
+    } catch (e) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Incorrect email or password."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    if (user.status != 'active') {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Your account status is '" + user.status + "'. Contact us for more information."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    // Update session
+    req.session.verification = {
+        'id': user.id,
+        'email': email,
+        'name': user.name,
+        'verified': true
+    };
+
+    res.write(JSON.stringify({
+        "success": true,
+        "user": user
+    }, null, "\t"));
+    res.end();
+}
+
+exports.loginByEmail = async (req, res, next) => {
+    const email = req.body.email
+
+    // Check login session
+    if (alreadyLogin(req, res, email)) return
+
+    var user;
+    try {
+        user = await User.findByEmail(email, 'login')
+    } catch {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Account is not exist."
+        }, null, "\t"));
+        res.end();
+        return
+    }
+
+    if (user.status != 'active') {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Your account status is '" + user.status + "'. Contact us for more information."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+    pendingLogin[email] = { 'email': email, 'code': verificationCode };
+
+    // Set expire
+    setTimeout((email, verificationCode) => {
+        if (!(email in pendingLogin)) return;
+        if (pendingLogin[email].code == verificationCode)
+            delete pendingLogin[email]
+    }, 5 * 60 * 1000, email, verificationCode);
+
+    emailSender.sendLoginVerification(email, verificationCode, function(error, info) {
+        if (error) {
+            res.write(JSON.stringify({
+                "success": false,
+                "message": "Unknown error. Please try again later."
+            }, null, "\t"));
+            res.end();
+            console.log(error);
+            return;
+        } else       
+            console.log(info.response);
+    });
+
+    // Add session
+    req.session.verification = {
+        'email': email,
+        'verified': false
+    };
+
+    res.write(JSON.stringify({
+        "success": true,
+        "message": "Email is sent"
+    }, null, "\t"));
+    res.end();
+
+    console.log('verificationCode', verificationCode);
+}
+
+exports.loginVerify = async (req, res, next) => {
+    const loginSession = req.session.verification;
+    if (!loginSession) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Session is not exist. Please login again."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    const email = loginSession.email
+    const code = req.body.code
+
+    if (!(email in pendingLogin)) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "The verification code is expired."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    if (code !== pendingLogin[email].code) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "The verification code is incorrect."
+        }, null, "\t"));
+        res.end();
+        console.log("The verification code is incorrect." + pendingLogin[email].code)
+        return;
+    }
+    delete pendingLogin[email]
+
+    // Get user information
+    var user;
+    try {
+        user = await User.findByEmail(email, 'login')
+    } catch (e) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Unknown error. Please try again later. " + e.message
+        }, null, "\t"));
+        res.end();
+        console.log("Unknown error."+pendingLogin[email].code)
+        return;
+    }
+    
+    // Update session
+    req.session.verification = {
+        'id': user.id,
+        'email': email,
+        'name': user.name,
+        'verified': true
+    };
+
+    res.write(JSON.stringify({
+        "success": true,
+        "user": user
+    }, null, "\t"));
+    res.end();
+}
+
+exports.logout = (req, res, next) => {
+    const loginSession = req.session.verification;
+    if (loginSession) 
+        delete req.session.verification
+
+    res.write(JSON.stringify({
+        "success": true
+    }, null, "\t"));
+    res.end();
+}
+
+exports.forgotPasswordEmail = async (req, res, next) => {
+    const email = req.body.email
+
+    var user;
+    try {
+        user = await User.findByEmail(email, 'login')
+    } catch {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Account is not exist."
+        }, null, "\t"));
+        res.end();
+        return
+    }
+
+    if (user.status != 'active') {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Your account status is '" + user.status + "'. Contact us for more information."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+    pendingResetPassword[email] = { 'email': email, 'code': verificationCode };
+
+    // Set expire
+    setTimeout((email, verificationCode) => {
+        if (!(email in pendingResetPassword)) return;
+        if (pendingResetPassword[email].code == verificationCode)
+            delete pendingResetPassword[email]
+    }, 5 * 60 * 1000, email, verificationCode);
+
+    emailSender.sendForgotPassword(email, verificationCode, function(error, info) {
+        if (error) {
+            res.write(JSON.stringify({
+                "success": false,
+                "message": "Unknown error. Please try again later."
+            }, null, "\t"));
+            res.end();
+            console.log(error);
+            return;
+        } else       
+            console.log(info.response);
+    });
+
+    // Add session
+    req.session.verification = {
+        'email': email,
+        'verified': false
+    };
+
+    res.write(JSON.stringify({
+        "success": true,
+        "message": "Email is sent"
+    }, null, "\t"));
+    res.end();
+
+    console.log('verificationCode', verificationCode);
+}
+
+exports.resetForgottenPassword = async (req, res, next) => {
+    const email = req.body.email
+    const code = req.body.code
+    const password = req.body.password
+
+    // Get user information
+    var user;
+    try {
+        user = await User.findByEmail(email, 'login')
+    } catch (e) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Account is not exist. "
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    if (!(email in pendingResetPassword)) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "The verification code is expired."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    if (code !== pendingResetPassword[email].code) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "The verification code is incorrect."
+        }, null, "\t"));
+        res.end();
+        console.log("The verification code is incorrect." + pendingResetPassword[email].code)
+        return;
+    }
+    delete pendingResetPassword[email]
+    
+    // Update session
+    user.password = password;
+    user.updatePassword();
+
+    res.write(JSON.stringify({
+        "success": true
+    }, null, "\t"));
+    res.end();
+}
+
+function userIsVerified(req, res) {
+    const loginSession = req.session.verification;
+    if (loginSession && loginSession.verified)   return true
+
+    res.write(JSON.stringify({
+        "success": false,
+        "message": "Please login"
+    }, null, "\t"));
+    res.end();
+    return false;
+}
+
+exports.getUserInfo = async (req, res, next) => {
+    var id = req.query.id
+    var user;
+
+    if (id == null) {
+        if (!userIsVerified(req, res)) return
+        id = req.session.verification.id
+    }
+
+    try {
+        user = await User.findById(id, 'query')
+    } catch (e) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Unknown error. " + e.message
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    // Remove password before response
+    delete user.password
+
+    res.write(JSON.stringify({
+        "success": true,
+        "user": user
+    }, null, "\t"));
+    res.end();
+    return;
+}
+
+exports.updateProfilePicture = async (req, res, next) => {
+    console.log(req.file)
+    const picUrl = "http://localhost:8080/user/profile/picture/" + req.file.filename
+
+    if (!userIsVerified(req, res)) return
+
+    const id = req.session.verification.id  // User Id
+    var user;
+    try {
+        user = await User.findById(id, 'update')
+    } catch (e) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Unknown error."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    user.picture = picUrl
+    user.updateProfilePicture()
+
+    res.write(JSON.stringify({
+        "success": true
+    }, null, "\t"));
+    res.end();
+}
+
+exports.updatePreferences = async (req, res, next) => {
+    if (!userIsVerified(req, res)) return
+
+    const id = req.session.verification.id  // User Id
+    var user;
+    try {
+        user = await User.findById(id, 'update')
+    } catch (e) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Unknown error."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+    // Please update value in user.preferences
+    // ...
+    // console.log(typeof(req.body.language));
+    // console.log(req.body.language);
+    const language = new Array(req.body.language);
+    const hobbies = new Array(req.body.hobbies);
+    user.preferences = language.concat(hobbies);
+    user.updatePreferences();
+    res.write(JSON.stringify({
+        "success": true
+    }, null, "\t"));
+    res.end();
+}
+
+exports.updateProfile = async (req, res, next) => {
+    if (!userIsVerified(req, res)) return
+
+    const id = req.session.verification.id  // User Id
+    var user;
+    try {
+        user = await User.findById(id, 'update')
+    } catch (e) {
+        res.write(JSON.stringify({
+            "success": false,
+            "message": "Unknown error."
+        }, null, "\t"));
+        res.end();
+        return;
+    }
+
+    // Password, Name...
+    const newName = req.body.name;
+    const newPassword = req.body.password;
+    if (newName!=""){
+        user.name = newName;
+        user.updateName();
+        res.write(JSON.stringify({
+            "success": true
+        }, null, "\t"));
+        res.end();
+        // console.log("update name");
+    }
+
+    if (newPassword!=""){
+        user.password = newPassword;
+        user.updatePassword();
+        res.write(JSON.stringify({
+            "success": true
+        }, null, "\t"));
+        res.end();
+        // console.log("update password");
+    }
+
+
+    
+}
