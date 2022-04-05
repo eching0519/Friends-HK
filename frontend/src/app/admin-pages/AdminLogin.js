2d7beb9f3fd06ce26d7fdb4588b03fcbb223f661
jhregan <84577611+jhregan@users.noreply.github.com>
2022-04-05 22:20:54 +0900

    gogogo

--- frontend/src/app/admin-pages/AdminLogin.js
+++ frontend/src/app/admin-pages/AdminLogin.js
@@ -1,8 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
+const querystring = require('querystring');
 
 const AdminLogin = (props) => {
+    const [adminName, setAdminName] = useState('');
+    const [adminPassword, setAdminPassword] = useState('');
+
+  const sendAdminLoginRequest = async () => {
+    let url = '/admin/login';
+
+    let res = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: querystring.stringify({
+          id : {adminName},
+          pw: {adminPassword}
+        })
+    });
+
+    let data
+    try {
+        data = await res.json();
+    } catch (error) {
+      props.setAlert({
+        visible: true,
+        strongMsg: 'Error!',
+        msg: `Unexpected error. (${error.message})`
+      })
+      return;
+    }
+    
+    if (!data.success) {
+        props.setAlert({
+          visible: true,
+          strongMsg: 'Sorry!',
+          msg: '${data.message}'
+      })
+      return;
+    }
+    else{
+      window.location.replace("/admin");
+    }
+  }
+
   return (
     <div>
       <div className="d-flex align-items-center auth px-0">
@@ -16,13 +57,13 @@ const AdminLogin = (props) => {
               <h6 className="font-weight-light">Sign in to continue.</h6>
               <Form className="pt-3">
                 <Form.Group className="d-flex search-field">
-                  <Form.Control type="email" placeholder="Username" size="lg" className="h-auto" />
+                  <Form.Control value={adminName} type="email" placeholder="Username" size="lg" className="h-auto" onChange={(event)=> setAdminName(event.target.value)}/>
                 </Form.Group>
                 <Form.Group className="d-flex search-field">
-                  <Form.Control type="password" placeholder="Password" size="lg" className="h-auto" />
+                  <Form.Control value = {adminPassword} type="password" placeholder="Password" size="lg" className="h-auto" onChange={(event)=> setAdminPassword(event.target.value)}/>
                 </Form.Group>
                 <div className="mt-3">
-                  <Link className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" to="/dashboard">SIGN IN</Link>
+                  <Link className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" to="/admin">SIGN IN</Link>
                 </div>
                 <div className="my-2 d-flex justify-content-between align-items-center">
                   <div className="form-check">
@@ -51,4 +92,4 @@ const AdminLogin = (props) => {
   )
 }
 
-export default AdminLogin
+export default AdminLogin;
