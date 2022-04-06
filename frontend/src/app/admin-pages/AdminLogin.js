0acff019b8525f4a4ae45487aa0b18e6e9d92819
eching0519 <eching0519@gmail.com>
2022-04-06 10:25:46 +0800

    Admin login

--- frontend/src/app/admin-pages/AdminLogin.js
+++ frontend/src/app/admin-pages/AdminLogin.js
@@ -1,11 +1,13 @@
 import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
+import AlertMessage from '../component/common/AlertMessage';
 const querystring = require('querystring');
 
 const AdminLogin = (props) => {
     const [adminName, setAdminName] = useState('');
     const [adminPassword, setAdminPassword] = useState('');
+    const [alert, setAlert] = useState({ visible: false, strongMsg: "123", msg: "456"});
 
   const sendAdminLoginRequest = async () => {
     let url = '/admin/login';
@@ -14,8 +16,8 @@ const AdminLogin = (props) => {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: querystring.stringify({
-          id : {adminName},
-          pw: {adminPassword}
+          id : adminName,
+          pw: adminPassword
         })
     });
 
@@ -23,7 +25,7 @@ const AdminLogin = (props) => {
     try {
         data = await res.json();
     } catch (error) {
-      props.setAlert({
+      setAlert({
         visible: true,
         strongMsg: 'Error!',
         msg: `Unexpected error. (${error.message})`
@@ -32,15 +34,17 @@ const AdminLogin = (props) => {
     }
     
     if (!data.success) {
-        props.setAlert({
+        setAlert({
           visible: true,
           strongMsg: 'Sorry!',
-          msg: '${data.message}'
+          msg: data.message
       })
       return;
     }
     else{
-      window.location.replace("/admin");
+      // window.location.replace("/admin");
+      sessionStorage.setItem('AdminProfile', JSON.stringify(data.admin));
+      window.location.pathname = "/admin";
     }
   }
 
@@ -53,17 +57,21 @@ const AdminLogin = (props) => {
               <div className="brand-logo">
                 <img src={require("../../assets/images/logo.svg")} alt="logo" />
               </div>
-              <h4>Hello! let's get started</h4>
-              <h6 className="font-weight-light">Sign in to continue.</h6>
-              <Form className="pt-3">
+              {alert.visible === true && <AlertMessage alert={alert} setAlert={setAlert} />}
+              <h4>Administrator Portal</h4>
+              <Form className="pt-3" onSubmit={(e) => {
+                e.preventDefault();
+                sendAdminLoginRequest();
+                return false;
+                }}>
                 <Form.Group className="d-flex search-field">
-                  <Form.Control value={adminName} type="email" placeholder="Username" size="lg" className="h-auto" onChange={(event)=> setAdminName(event.target.value)}/>
+                  <Form.Control value={adminName} type="text" placeholder="Username" size="lg" className="h-auto" onChange={(event)=> setAdminName(event.target.value)}/>
                 </Form.Group>
                 <Form.Group className="d-flex search-field">
                   <Form.Control value = {adminPassword} type="password" placeholder="Password" size="lg" className="h-auto" onChange={(event)=> setAdminPassword(event.target.value)}/>
                 </Form.Group>
                 <div className="mt-3">
-                  <Link className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" to="/admin">SIGN IN</Link>
+                  <button className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">SIGN IN</button>
                 </div>
                 <div className="my-2 d-flex justify-content-between align-items-center">
                   <div className="form-check">
@@ -73,15 +81,6 @@ const AdminLogin = (props) => {
                       Keep me signed in
                     </label>
                   </div>
-                  <a href="!#" onClick={event => event.preventDefault()} className="auth-link text-black">Forgot password?</a>
-                </div>
-                <div className="mb-2">
-                  <button type="button" className="btn btn-block btn-facebook auth-form-btn">
-                    <i className="mdi mdi-facebook mr-2"></i>Connect using facebook
-                  </button>
-                </div>
-                <div className="text-center mt-4 font-weight-light">
-                  Don't have an account? <Link to="/user/register" className="text-primary">Create</Link>
                 </div>
               </Form>
             </div>
