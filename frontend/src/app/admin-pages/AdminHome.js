391b16a1d197d17b0835a80288ca526515f86013
eching0519 <eching0519@gmail.com>
2022-04-06 10:42:24 +0800

    Admin login

--- frontend/src/app/admin-pages/AdminHome.js
+++ frontend/src/app/admin-pages/AdminHome.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import LoginVerifier from '../component/common/LoginVerifier'
 
 const AdminHome = (props) => {
+    let admin = JSON.parse(sessionStorage.getItem('AdminProfile'));
     return (
         <>
+            <h3>Hi {admin.name}. Welcome back!</h3>
         </>
     );
 }
