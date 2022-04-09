a0e2ee1eba23d766634ca0de374f2f28ef2514da
iamissacyip <yipchiwaiissac@gmail.com>
2022-04-10 00:07:58 +0800

    update WUR

--- frontend/src/app/user-panel/WRUgame/WRUgame.js
+++ frontend/src/app/user-panel/WRUgame/WRUgame.js
@@ -1,37 +1,64 @@
 import React, { useEffect, useState } from "react";
+import { useContext } from "react";
 import { io } from 'socket.io-client';
+import SocketContext from "../../SocketContext";
 
-const socket = io({ //no url: default to localhost:8080
+/* const socket = io({ //no url: default to localhost:8080
     autoConnect: false
-});
+}); */
 
 const WRUgame = (props) => {
+    const socket = useContext(SocketContext);
     const [question, setQuestion] = useState('');
     const [answerA, setanswerA] = useState('');
     const [answerB, setanswerB] = useState('');
+    const [answerAuserName, setAnswerAuserName] = useState([]);
+    const [answerBuserName, setAnswerBuserName] = useState([]);
 
     useEffect(() => {
-        socket.connect();   //estiblish socket io connection
+        console.log(socket);
+        //socket.connect();   //estiblish socket io connection
 
-        socket.emit("wouldURgame", props.userName, props.roomId);
+        socket.emit("joinWouldURgame", props.userName, props.roomId, (message) => {
+            console.log(message);
+        });
 
-        socket.on("wouldURgameSession", (question) => {
+        socket.on("assignWouldURgameQuestion", (question) => {
             setQuestion(question.question);
             setanswerA(question.a);
             setanswerB(question.b);
         });
 
-        return () => {
-            socket.removeAllListeners();    //clean up listener
-            socket.disconnect();    //disconnect socket io connection
-        }
+        socket.on("waitResponseUserName", (name, answer) => {
+            if (answer === 'a') {
+                setAnswerAuserName([...answerAuserName, name]);
+            }
+
+            if (answer === 'b') {
+                setAnswerBuserName([...answerBuserName, name]);
+            }
+
+        });
+
+        // return () => {
+        //     socket.removeAllListeners();    //clean up listener
+        //     socket.disconnect();    //disconnect socket io connection
+        // }
     }, []);
 
     const sendWRUanswer = (answer) => {
-        socket.emit("sendWouldURanswer", props.userName, props.roomId, answer, () => {
-
+        socket.emit("sendWouldURanswer", props.userName, props.roomId, answer, (message) => {
+            console.log(message);
         });
     };
+
+    // const renderUserAnswerList = (list) => {
+    //     return list.map((element, index) => {
+    //         <div key={index}>
+    //             {element}
+    //         </div>
+    //     })
+    // };
 
     return (
         <div className="alert alert-success" role="alert">
@@ -47,6 +74,7 @@ const WRUgame = (props) => {
                 <button type="button" className="btn btn-rounded btn-gradient-primary col-auto" onClick={event => {
                     sendWRUanswer('a');
                 }}>{answerA}</button>
+                {/* {renderUserAnswerList} */}
                 <button type="button" className="btn btn-rounded btn-gradient-primary col-auto" onClick={event => {
                     sendWRUanswer('b');
                 }}>{answerB}</button>
